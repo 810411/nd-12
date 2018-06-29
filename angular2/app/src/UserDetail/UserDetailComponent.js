28d53598029065c48a0d1db7da7c03020f0efd2e
GREEV\greev <810411@mail.ru>
2018-06-29 14:48:51 +0500

    !

---
+++ angular2/app/src/UserDetail/UserDetailComponent.js
@@ -0,0 +1,52 @@
+'use strict';
+
+userApp.component('userDetail', {
+
+	bindings: {
+		user: '<'
+	},
+	
+	controller: function UserDetailCtrl($routeParams, UsersService) {
+
+		let vm = this;
+
+		vm.$onInit = function() {
+			vm.userLoaded = false;
+
+			vm.user = UsersService.get({
+				userId: $routeParams['userId']
+			}, function(successResult) {
+				// Окей!
+				vm.notfoundError = false;
+				vm.userLoaded = true;
+
+				vm.activeTab = 0;
+				vm.disableControlTab = false;
+
+				console.log(vm.user);
+			}, function(errorResult) {
+				// Не окей..
+				vm.notfoundError = true;
+				vm.userLoaded = true;
+			});
+
+			vm.user.$promise.then(function(result) {
+				//vm.userLoaded = true;
+			});
+		};
+
+		vm.deleteUser = function(userId) {
+			vm.user.$delete({
+				userId: userId
+			}, function(successResult) {
+				// Окей!
+				vm.deletionSuccess = true;
+			}, function(errorResult) {
+				// Не окей..
+				vm.deletionError = true;
+			});
+		}
+	},
+
+	templateUrl: './src/UserDetail/UserDetail.html'
+});
